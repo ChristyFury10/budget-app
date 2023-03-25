3ad1e41776f7675ece6b9ac79a10fad149af7e39
Christy Fury <christy.fury@gmail.com>
2023-03-25 09:48:12 -0400

    creates budget table on index

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const Budget = require("./models/budget.js")
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended:false }));
 
 app.get("/", (req, res)=>{
-    res.send("working")
+    // res.send("working")
+    res.render("index.ejs", {Budget})
 })
 
 app.listen(PORT, () => {
